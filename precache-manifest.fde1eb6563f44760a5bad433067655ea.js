e50c29532b1ef25426a90473bc75de036c2bbcce
fuckmylife <98694702+fuxkmylife@users.noreply.github.com>
2022-02-24 11:34:20 +0800

    Add files via upload

---
+++ precache-manifest.fde1eb6563f44760a5bad433067655ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fb7b531b76a8f68af9e5af7427407d",
+    "url": "/ShamanzsFanGame/index.html"
+  },
+  {
+    "revision": "33637941f3aa9f973a92",
+    "url": "/ShamanzsFanGame/static/css/2.f1bd3b54.chunk.css"
+  },
+  {
+    "revision": "7b30111d4e468aedf43e",
+    "url": "/ShamanzsFanGame/static/css/main.b18b17fe.chunk.css"
+  },
+  {
+    "revision": "33637941f3aa9f973a92",
+    "url": "/ShamanzsFanGame/static/js/2.2cf41ecc.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/ShamanzsFanGame/static/js/2.2cf41ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b30111d4e468aedf43e",
+    "url": "/ShamanzsFanGame/static/js/main.ea0f2f02.chunk.js"
+  },
+  {
+    "revision": "50abcac0bb61588b725c",
+    "url": "/ShamanzsFanGame/static/js/runtime-main.01d6bd2d.js"
+  },
+  {
+    "revision": "2d25496d84605cc839b793d31bb299ac",
+    "url": "/ShamanzsFanGame/static/media/Shamanzs_game_2-04-min.2d25496d.png"
+  },
+  {
+    "revision": "212c68d377be98f6925a0808703281f5",
+    "url": "/ShamanzsFanGame/static/media/Shamanzs_game_2-06-min.212c68d3.png"
+  },
+  {
+    "revision": "8597153b49c5ba2852081707a67b35a6",
+    "url": "/ShamanzsFanGame/static/media/Shamanzs_game_2-07-min.8597153b.png"
+  }
+]);
